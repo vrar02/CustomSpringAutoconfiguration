8f5e22d8f224cf2d20ec7ace2180d2fce484933c
Vivek Reddy <vivekanthireddy02@gmail.com>
2023-03-08 01:53:39 -0600

    spring boot autoconfiguration project to autoconfigure the HelloService with default implementation..
    will take dependency of helloservice artifactId and to make this springboot project used as dependency in Test application change the build plugin in pom to exec

---
+++ hello-service-autoconfigure/src/main/java/com/vivek/helloserviceautoconfigure/HelloServiceAutoConfigureApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.helloserviceautoconfigure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServiceAutoConfigureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServiceAutoConfigureApplication.class, args);
+	}
+
+}
---
+++ hello-service-autoconfigure/src/test/java/com/vivek/helloserviceautoconfigure/HelloServiceAutoConfigureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.helloserviceautoconfigure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloServiceAutoConfigureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
